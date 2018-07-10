2169e847f8b06f3cdc8716f1638ba1695976cf80
Daniel Horton-Wellings <danielhortonwellings@gmail.com>
2018-07-10 16:23:30 -0500

    Update task scripts

--- docker/tasks/shell.sh
+++ docker/tasks/shell.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 cd ../
+source ../../.env
 if [ -z "$1" ]
   then
     docker-compose -p $PROJECT_NAME exec --user www-data magento bash
--- tasks/magento.sh
+++ tasks/magento.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 source ../../.env
 cd ../docker
+
 docker-compose -p $PROJECT_NAME exec --user www-data magento /var/www/html/bin/magento $@
