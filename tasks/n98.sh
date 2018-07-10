c817ee6ee42447d9aca6a5394386089c867d8c0b
Daniel Horton-Wellings <danielhortonwellings@gmail.com>
2018-07-10 15:23:45 -0500

    Added tasks

---
+++ tasks/n98.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+source ../../.env
+cd ../docker
+docker-compose -p $PROJECT_NAME exec --user www-data magento n98-magerun2.phar $@
---
+++ tasks/xdebug.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+source ../../.env
+cd ../docker
+docker-compose -p $PROJECT_NAME exec --user root magento xdebug
