1fcef09b8eb5fad52e8a451e7c4023424150efde
Daniel Horton-Wellings <danielhortonwellings@gmail.com>
2018-07-10 14:51:21 -0500

    Added docker build for apache container + tasks

--- docker/tasks/shell.sh
+++ docker/tasks/shell.sh
@@ -2,7 +2,7 @@
 cd ../
 if [ -z "$1" ]
   then
-    docker-compose exec --user www-data magento bash
+    docker-compose -p $PROJECT_NAME exec --user www-data magento bash
 else
-    docker-compose exec --user $@ magento bash
+    docker-compose -p $PROJECT_NAME exec --user $@ magento bash
 fi
