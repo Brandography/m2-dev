e1a2952a97ccf1a3b65f1924ddec0124916ca37b
Daniel Horton-Wellings <danielhortonwellings@gmail.com>
2018-07-25 13:46:58 -0500

    Updated init script to include tags

--- changelog.md
+++ changelog.md
@@ -5,9 +5,13 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+
+## 0.1.0 - 2018-07-25
 ### Added
 - Initial changelog input
 - MIT License
 
+### Changed
+- Updated init script to include dev tag version + master for dev purposes
 
-[Unreleased]: https://github.com//brandography/m2-dev/compare/0.2.1...HEAD
+[Unreleased]: https://github.com//brandography/m2-dev/compare/0.1.0...HEAD
--- init.sh
+++ init.sh
@@ -1,19 +1,42 @@
 #!/usr/bin/env bash
 if [ -z "$1" ]; then
     echo "Enter your project name"
+	exit 0;
 else
     DEVDIR=$(dirname "$0")
+	cd $DEVDIR
+
+	if [ "$2" = "master" ]; then
+		git checkout master
+		git pull origin master
+	else
+		git fetch --tags
+		LATEST_TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
+		echo "Updating to the latest version $LATEST_TAG"
+		git checkout $LATEST_TAG
+
+		if [ -f "../.env" ]; then
+			source ../.env
+			if [ $DEV_VERSION = $LATEST_TAG ] && [ $PROJECT_NAME = $1 ]; then
+				echo "The dev & deploy environment is at the latest version $LATEST_TAG and the project name is the same."
+				echo "No need for an update."
+				exit 0;
+			fi
+		fi
+	fi
+
     echo "Initializing Project $1!"
 
-    cp $DEVDIR/.env.sample .env
-    sed -i "s/PROJECT_NAME=.*/PROJECT_NAME=$1/" .env
+    cp .env.sample ../.env
+    sed -i "s/PROJECT_NAME=.*/PROJECT_NAME=$1/" ../.env
+	sed -i "s/DEV_VERSION=.*/DEV_VERSION=$LATEST_TAG/" ../.env
 
-    if [ ! -d "src" ]; then
+    if [ ! -d "../src" ]; then
         echo "Creating src dir."
-        mkdir src
+        mkdir ../src
     fi
 
-    cp $DEVDIR/Makefile.sample ./Makefile
-    sed -i '1iDEV_DIR='$DEVDIR Makefile
-    sed -i '1iPROJECT_NAME='$1 Makefile
+    cp Makefile.sample ../Makefile
+    sed -i '1iDEV_DIR='$DEVDIR ../Makefile
+    sed -i '1iPROJECT_NAME='$1 ../Makefile
 fi
